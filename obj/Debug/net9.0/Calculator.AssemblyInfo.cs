567b3010ce7df6a7558f6101843ac0b22b3be760
masmith616 <m.a.smith0616@gmail.com>
2025-09-08 13:02:41 -0700

    ci: add GitHub Actions workflow for build & test

--- obj/Debug/net9.0/Calculator.AssemblyInfo.cs
+++ obj/Debug/net9.0/Calculator.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Calculator")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+52e6b9d48923ea16f9b7bfc855da57700c5906ba")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f7b082dc59993937099738007c9f174bbbea3d16")]
 [assembly: System.Reflection.AssemblyProductAttribute("Calculator")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Calculator")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
